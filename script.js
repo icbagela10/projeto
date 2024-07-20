2da1236d11e8db69957a9ea624be0d0478751662
icbagela10 <icbagela10@gmail.com>
2024-07-20 03:27:31 -0300

    projeto do curso

---
+++ script.js
@@ -0,0 +1,34 @@
+ function toggleMode() {
+   const html = document.documentElement
+   html.classList.toggle('light')
+
+   /*if(html.classList.contains('light')) {
+    html.classList.remove('light')
+   } else {
+    html.classList.add('light')
+   }*/
+
+/* o toggle faz tudo isso ai em cima*/
+
+  // pegar a tag img
+
+    const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+
+  if(html.classList.contains('light')) {
+
+ // se tiver light mode, adicionar a imagem light  
+    img.setAttribute('src','./assets/Avatar2.png')
+
+  } else {
+    
+// se tiver sem lght mode, manter a imagem normal
+
+  img.setAttribute('src' ,'./assets/avatar.png')
+  }
+
+    }
+
+
+ 
